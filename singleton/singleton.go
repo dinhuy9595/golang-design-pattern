ac696908920e4de0a2b5a33b3fbdcf59583f301a
yuu <dinhuy9595@gmail.com>
2021-09-06 00:49:14 +0700

    add singleton and prototype

---
+++ singleton/singleton.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var lock = &sync.Mutex{}
+
+type singleton struct {
+}
+
+var singletonInstance *singleton
+
+func getInstance() *singleton {
+	if singletonInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if singletonInstance == nil {
+			fmt.Println("Creating singleton instance now.")
+			singletonInstance = &singleton{}
+		} else {
+			fmt.Println("Singletion instance already created.")
+		}
+	} else {
+		fmt.Println("Singleton instance already created.")
+	}
+	return singletonInstance
+}
